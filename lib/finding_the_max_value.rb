7ea6485d800a8e0b77a9993d0e524335fa2a9c19
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-13 16:48:40 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,13 @@
 def find_max_value(array)
-  # Add your solution here
+  count = 0
+  max_value = array[0]
+  while count < array.length do
+    if max_value > array[count]
+      max_value
+      count += 1
+      return max_value
+    else
+      max_value = array[count]
+      count += 1
+      return max_value
 end
