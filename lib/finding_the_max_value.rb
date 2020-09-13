2cb988caf50e4a3dcc95c20c0a2f4e428254d0c4
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-13 16:49:48 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,7 @@ def find_max_value(array)
   count = 0
   max_value = array[0]
   while count < array.length do
-    if max_value > array[count]
+    if max_value >= array[count]
       max_value
       count += 1
       return max_value
